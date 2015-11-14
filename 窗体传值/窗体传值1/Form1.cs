e380732afea5dc1ed70354748d41e4043f5d2893
lishuanguan1987 <294388344@qq.com>
2015-11-14 22:33:31 +0800

    窗体传值练习

---
+++ 窗体传值/窗体传值1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 窗体传值1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 f;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            f = new Form2();
+            f.textBox1.TextChanged += textBox1_TextChanged;
+            f.ShowDialog();
+        }
+
+        void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.textBox1.Text = f.textBox1.Text;
+        }
+    }
+}
