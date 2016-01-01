360b553c949e4de1563e1308a289701b5ce5c917
lishuanguan1987 <294388344@qq.com>
2016-01-01 23:08:47 +0800

    no message

---
+++ Test/Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int temp = p.Add(2, 3);
+            Console.WriteLine("结果是："+temp.ToString());
+            Console.Read();
+        }
+        int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+}
